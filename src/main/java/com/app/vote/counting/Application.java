28c40abc3f76704f2af3192fa2ffac6c9c64b878
Kaleem Brohi <kaleem.brohi27@gmail.com>
2022-04-06 14:28:26 -0500

    Data setup inserted by users

--- src/main/java/com/app/vote/counting/Application.java
+++ src/main/java/com/app/vote/counting/Application.java
@@ -1,5 +1,9 @@
 package com.app.vote.counting;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import com.app.vote.counting.util.Utils;
 import com.app.vote.counting.util.VotingOptionsFileLoader;
 
@@ -13,6 +17,27 @@ public class Application {
 		System.out.println(options);
 		
 		//display options with prefix
-		System.out.println(Utils.getOptionsWithPrefix(options);
+		System.out.println(Utils.getOptionsWithPrefix(options));
+		
+		Map<String, Map<String, Integer>> ballots = new HashMap<String, Map<String,Integer>>();
+		Map<Character, String> places = Utils.options(options);
+		
+		int count = 1;
+		Scanner scan = new Scanner(System.in);
+		String input;
+		while(!(input = scan.next()).equalsIgnoreCase("tally")) {
+			Map<String, Integer> values = new HashMap<String, Integer>();
+			for(int i = 0; i < input.length(); i++) {
+				Character ch  = input.charAt(i);
+				values.put(places.get(ch), i + 1);
+			}
+			
+			ballots.put("Candidate " + count, values);
+			
+			count++;
+		}
+		
+
+		String winner = Utils.findWinner(ballots);
 	}
 }
--- src/main/java/com/app/vote/counting/util/Utils.java
+++ src/main/java/com/app/vote/counting/util/Utils.java
@@ -1,7 +1,9 @@
 package com.app.vote.counting.util;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Utils {
 
@@ -13,10 +15,38 @@ public class Utils {
 		int prefix = 65;
 		
 		for(String option : optionsList) {
-			result += (char)prefix + option+"\n";
+			result += (char)prefix + ". " + option+"\n";
 			prefix++;
 		}
 		return result;
+	}
+	
+	public static Map<Character, String> options(String options) {
+		Map<Character, String> places = new HashMap<Character, String>();
+		List<String> optionsList = Arrays.asList(options.split("\n"));
+		
+		int prefix = 65;
+		
+		for(String option : optionsList) {
+			places.put((char)prefix, option);
+			prefix++;
+		}
+		
+		return places;
+	}
+
+	public static String findWinner(Map<String, Map<String, Integer>> ballots) {
+		for (Map.Entry<String, Map<String, Integer>> ballot : ballots.entrySet()) {
+			
+			System.out.println(ballot.getKey());
+			for(Map.Entry<String, Integer> candidate : ballot.getValue().entrySet()) {
+				System.out.println(candidate.getKey() + " : " + candidate.getValue());
+			}
+			
+			System.out.println("**********************************************************");
+		}
+
+		return null;
 	}
 	
 }
