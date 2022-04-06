751b12ac53d90d639dcb0d21f8aeb3b5b37f49e4
Kaleem Brohi <kaleem.brohi27@gmail.com>
2022-04-06 11:12:49 -0500

    configuration changes

--- src/main/java/com/app/vote/counting/Application.java
+++ src/main/java/com/app/vote/counting/Application.java
@@ -1,5 +1,6 @@
 package com.app.vote.counting;
 
+import com.app.vote.counting.util.Utils;
 import com.app.vote.counting.util.VotingOptionsFileLoader;
 
 public class Application {
@@ -11,5 +12,7 @@ public class Application {
 		String options = votingOptionsFileLoader.loadOptions();
 		System.out.println(options);
 		
+		//display options with prefix
+		System.out.println(Utils.getOptionsWithPrefix(options);
 	}
 }
--- src/main/java/com/app/vote/counting/util/Utils.java
+++ src/main/java/com/app/vote/counting/util/Utils.java
@@ -1,5 +1,22 @@
 package com.app.vote.counting.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Utils {
 
+	public static String getOptionsWithPrefix(String options) {
+		
+		List<String> optionsList = Arrays.asList(options.split("\n"));
+		
+		String result = "";
+		int prefix = 65;
+		
+		for(String option : optionsList) {
+			result += (char)prefix + option+"\n";
+			prefix++;
+		}
+		return result;
+	}
+	
 }
